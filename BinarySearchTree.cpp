7be438e5f40bd5f2f38c02549723ce126ab01d6f
dzakyputra45 <dzakypratama45@gmail.com>
2024-06-13 16:14:37 +0700

    Case 5 dan default

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -156,12 +156,20 @@ int main()
             x.preorder(x.ROOT);
             break;
         }
-
-
-
-
-
-
+        case '4':
+        {
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "invalid option" << endl;
+            break;
+        }
         }
     }
 }
