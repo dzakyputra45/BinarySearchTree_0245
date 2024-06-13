9e4136d2eb37760e4217752d53ab04c10a7c638d
dzakyputra45 <dzakypratama45@gmail.com>
2024-06-13 16:06:16 +0700

    Case 1 'Enter a Word'

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -135,5 +135,17 @@ int main()
         char ch;
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        }
     }
 }
